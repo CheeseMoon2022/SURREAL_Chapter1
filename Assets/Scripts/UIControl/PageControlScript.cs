5ab1b172fcc1eda45675f9993cf44aa975dfee95
焗焗 <839191793@qq.com>
2022-02-27 16:12:26 +0800

    2.27场景更新
    引导场景逻辑重做，章节选择场景制作中

---
+++ Assets/Scripts/UIControl/PageControlScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageControlScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //该脚本用于在章节选择中控制左右翻页的逻辑
+    public int pageNum = 3;
+
+   
+}
